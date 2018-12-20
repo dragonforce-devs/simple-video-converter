583fbe6090864762bb336bfb5f74bafaebb80976
Dipanjan Patra <patraanjan23@gmail.com>
2018-12-20 20:53:18 +0530

    added file picker dialog
    QListView not yet implemented for showing the files
    but the program currently prints out the files selected in terminal

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ This is a video converter
 """
 import sys
 
-from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtWidgets import QApplication, QWidget, QFileDialog
 from PySide2 import QtGui, QtWidgets
 
 from Form import Ui_Form
@@ -14,16 +14,18 @@ class VidConvertWindow(QWidget, Ui_Form):
     main class
     """
     item = ''
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # "self.listWidget_2.itemSelectionChanged.connect(self.itemActivated_event)"
+
+        self.filePicker = QFileDialog(self)
+
         self.btnStop.clicked.connect(self.stop_convertion)
         self.btnAdd.clicked.connect(self.add_files)
         self.btnConvert.clicked.connect(self.start_convertion)
         self.post_init()
         self.addVideoProfiles()
-
 
     """
     def itemActivated_event(self):
@@ -40,18 +42,24 @@ class VidConvertWindow(QWidget, Ui_Form):
 
         self.setWindowTitle("Simple Video Converter")
         self.setGeometry(100, 100, 640, 480)
-        
-        self.btnAdd.setFixedSize(50,30)
-        self.btnConvert.setFixedSize(50,30)
-        self.btnStop.setFixedSize(50,30)
-        
+
+        # self.btnAdd.setFixedSize(50,30)
+        # self.btnConvert.setFixedSize(50,30)
+        # self.btnStop.setFixedSize(50,30)
 
     def add_files(self):
+        self.filePicker.setFileMode(QFileDialog.ExistingFiles)
+        self.filePicker.setNameFilter("Videos (*.mp4 *.mkv *.mov)")
+        self.filePicker.setViewMode(QFileDialog.Detail)
+        if self.filePicker.exec_():
+            files_selected = self.filePicker.selectedFiles()
+            for f in files_selected:
+                print(f)
         pass
-    
+
     def start_convertion(self):
         pass
-    
+
     def stop_convertion(self):
         pass
 
@@ -69,10 +77,11 @@ class VidConvertWindow(QWidget, Ui_Form):
 
     def addVideoProfiles(self):
         pass
+
     #     """
     #     Here we add the format list items
     #     """
-               
+
     #     k=1
     #     format = ['AVI', 'MP4', 'WMV']
     #     for i in format:
@@ -80,7 +89,6 @@ class VidConvertWindow(QWidget, Ui_Form):
     #             item = str(k)+')'+' '+i+' ' +str(j)
     #             k = k+1
     #             self.listWidget_format.addItem(item)
-
 
     # def open(self):
 
@@ -91,7 +99,7 @@ class VidConvertWindow(QWidget, Ui_Form):
     #     loc, _ = QtWidgets.QFileDialog.getOpenFileNames(None,"Open File","", "Videos (*.mp4 *.avi *.wmv *.mkv *.flv *.dat);;All Files(*.*)")
     #     for i in loc:
     #         self.listWidget_files.addItem(i)
-    
+
 
 if __name__ == "__main__":
     APP = QApplication(sys.argv)
